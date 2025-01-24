475ff7bf546825221c7e228820cdea5b15573b02
iseed3adpe0ple <d_bedelov@kbtu.kz>
2025-01-25 01:05:11 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,80 @@
+# 1
+# print("Hello, world")
+
+# 2
+# import sys
+# print(sys.version)
+
+# 3
+# if 5 > 2:
+#     print("Five is greater than two!")  
+
+
+# 4
+# This is a comment
+# written in
+# more than just one line
+
+
+#5
+# x = 5
+# y = "John"
+# print(type(x))
+# print(type(y))
+
+
+# 6
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+
+# 7
+# x = "Python "
+# y = "is "
+# z = "awesome"
+# print(x + y + z)
+
+
+
+# 8
+# x = "awesome"
+
+# def myfunc():
+#   global x
+#   x = "fantastic"
+
+# myfunc()
+
+# print("Python is " + x)
+
+
+# 9
+# x = 1
+# y = 35656222554887711
+# z = -3255522
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+
+#10
+# b = "Hello, World!"
+# print(b[-5:-2])
+
+
+#11
+# a = " Hello, World! "
+# print(a.strip().lower())
+
+
+#12 
+# price = 59
+# txt = f"The price is {price} dollars"
+# print(txt)
+
+
+#13
+txt = "We are the so-called \"Vikings\" from the north."
